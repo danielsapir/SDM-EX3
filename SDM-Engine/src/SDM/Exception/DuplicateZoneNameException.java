8e0d4d52a0f927f222bd444087ecf093db227dd7
noy elimelech <noyel@mta.ac.il>
2020-11-11 20:02:48 +0200

    .

--- SDM-Engine/src/SDM/Exception/DuplicateZoneNameException.java
+++ SDM-Engine/src/SDM/Exception/DuplicateZoneNameException.java
@@ -2,9 +2,15 @@ package SDM.Exception;
 
 
 //noy 11/11
-public class DuplicateZoneNameException
+public class DuplicateZoneNameException extends Exception
 {
+    private String zoneName;
+    public DuplicateZoneNameException(String name) {
+        this.zoneName = name;
+    }
 
-
+    public String getZoneName() {
+        return zoneName;
+    }
 
 }
